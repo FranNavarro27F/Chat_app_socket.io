abe9e84fc78b94662b064cebba90bae74b6f6154
Francisco Navarro <fran27_navarro@hotmail.com>
2023-03-07 16:53:52 -0300

    chat version 1.0.0

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,84 @@
-console.log('Hello from foods template!');
+/**
+ * io hace referencia a socket.io, se llama así por convención
+ * Esta línea permite instanciar el socket del lado del cliente, y almacenarlo en una variable
+ * Dicho socket es el que utilizaremos para comunicarnos con el servidor
+ * (Recuerda que este código se ejecuta en el navegador del cliente, dentro de nuestra vista).
+ */
+// Swal.fire({
+//     title: 'Hola, coders',
+//     text: 'Alerta básica con SweetAlert2',
+//     icon: 'success'
+// });
+
+console.log('Hello, from Soket.io_Client');
+
+const socket= io();
+
+// Declaramos la variable user para alojar nuestro usuario una vez que se identifique
+let user;
+
+Swal.fire({
+    title: "identificate",
+    input: "text", // Especificamos que necesitamos de un input de tipo texto.
+    text: "Ingresa el usuario para identificarte en el chat",
+    // La siguiente función  definira una varlidacion para el input.
+    // en nuestro caso verificaremos que el nombre de usuario ingresado no esté vacío
+    inputValidator: (value) => {
+        return !value && "¡Necesitas escribir un nombre de usuario para continuar!";
+    },
+    // Esta opción evita que el usuario pueda cerrar el dialogo haciendo click fuera de él.
+    allowOutsideClick: false,
+}).then((result) => {
+    // Una vez que el usuario ungresa un nombre, lo asignamos a la variable user.
+    user= result.value;
+});
+
+// Obtenemos una referencia al elemento con id chat-box
+const chatBox= document.getElementById("chat-box");
+const sendButton= document.getElementById("send");
+
+/**
+ * Agregamos un event listener, que nos permitirá escuchar eventos del navegador
+ * En este caso, escucharemos el evento keyup, que se dispara cada vez que una tecla se suelta.
+ */
+//----------------------------------------------------
+// chatBox.addEventListener("keyup", (ev) => {
+    //     /**
+    //      * Si la tecla presionada es Enter y la longitud del mensaje es mayor a 0, enviamos el mensaje y limpiamos el input.
+    //      */
+//     if (ev.key === "Enter") {
+//         console.log("touch Enter key")
+//         const message= chatBox.value;
+
+//         if(message.trim().length > 0) {
+//             socket.emit("chat-message", { user, message });
+//             chatBox.value= "";
+//         }
+//     }
+// });
+//----------------------------------------------------
+
+//----------------------
+sendButton.addEventListener("click", (ev) => {
+    const message= chatBox.value;
+
+    if(message.trim().length > 0) {
+        socket.emit("chat-message", { user, message });
+        chatBox.value= "";
+    }
+});
+//----------------------
+
+
+// Obtenemos una referencia al elemento con id message-logs.
+const messageLogs= document.getElementById("message-logs");
+
+// Cuando recibimos la lista actualizada de mensajes, los agregamos en este elemento, uno debajo del otro
+socket.on("message-logs", (data) => {
+    console.log("escucho el evento de mensaje que se mando")
+    let messages= "";
+    data.forEach((message) => {
+        messages += `<b>${message.user}</b>: ${message.message}</br>`;
+    });
+    messageLogs.innerHTML= messages;
+});
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import __dirname from './utils.js';
+import { __dirname } from './utils.js';
 import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
 
 
 const app= express();
@@ -9,14 +10,32 @@ const app= express();
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname+'/views');
 app.set('view engine', 'handlebars');
-
 app.use(express.static(__dirname + "/../public"))
+
 
 app.use('/views', viewsRouter);
 
 
-
-const server= app.listen(8080, () => {
+const httpServer= app.listen(8080, () => {
     console.log('Listen on PORT 8080');
 });
  
+const io= new Server(httpServer);
+
+let messages= [];// Array donde almacenaremos los mensajes.
+
+io.on("connection", (soket) => {
+    console.log(`Nuevo cliente conectado: ${soket.id}`);
+
+    /**
+     * Escuchamos eventos de tipo 'chat-message' (recordemos que es el mismo nombre que usamos en el cliente. Cliente=[/public/js/index.js]).
+     */
+
+    soket.on("chat-message", (data) => {
+        // Actualizamos los mensajes
+        messages= [...messages, data];
+
+        // Emitimos un nuevo evento de tipo 'message-logs' con los mensajes actualizados.
+        io.emit("message-logs", messages);
+    });
+});
